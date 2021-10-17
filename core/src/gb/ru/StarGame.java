9a3e33d32ea7e67595add2a3582f64ebfb09b659
Andre <ncl8@mail.ru>
2021-10-17 09:59:09 +0300

    Урок 2 часть 1

--- core/src/gb/ru/StarGame.java
+++ core/src/gb/ru/StarGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
@@ -16,22 +17,48 @@ public class StarGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 20, 25, 100, 100);
+
+		Vector2 v1 = new Vector2(2, 2);
+		Vector2 v2 = new Vector2();
+
+		v2.set(2, 1);
+		v1.add(v2);
+		System.out.println("v1.add(v2) v1.x = " + v1.x + " v1.y = " + v1.y);
+
+		v1.set(3, 7);
+		v2.set(6, 2);
+		v1.sub(v2);
+		System.out.println("v1.sub(v2) v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("v1.len() = " + v1.len());
+
+		v1.setLength(100);
+		System.out.println("v1.setLength(100) v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("v1.len() = " + v1.len());
+		v1.scl(0.95f);
+		System.out.println("v1.len() = " + v1.len());
+		v1.nor();
+		System.out.println("v1.len() = " + v1.len());
+
+		System.out.println("v1.x = " + v1.x + " v1.y = " + v1.y);
+		Vector2 v3 = v1.cpy().add(v2);
+		System.out.println("v1.add(v2) v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("v3.add(v2) v3.x = " + v3.x + " v3.y = " + v3.y);
+
+		v1.set(1, 1);
+		v2.set(-1, 1);
+		v1.nor();
+		v2.nor();
+		System.out.println(Math.acos(v1.dot(v2)));
+
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0.5f, 0.23f,0.74f, 1);
 		batch.begin();
-		batch.setColor(1, 1, 1, 0.5f);
-		batch.draw(img, x, y, 200, 200);
-		batch.setColor(1, 1, 1, 1f);
-		batch.draw(region, 300, 300);
-		batch.setColor(1, 1, 1, 0.5f);
-		batch.draw(region, 200, 200);
+		batch.draw(img, x, y);
 		batch.end();
-		x++;
-		y++;
 	}
 	
 	@Override
