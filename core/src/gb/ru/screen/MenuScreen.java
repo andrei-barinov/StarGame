f0704e7f8cde084542f0cd1e65482afee21c463a
Andre <ncl8@mail.ru>
2021-10-21 22:32:28 +0300

    Урок 3 часть 1

--- core/src/gb/ru/screen/MenuScreen.java
+++ core/src/gb/ru/screen/MenuScreen.java
@@ -11,30 +11,21 @@ public class MenuScreen extends BaseScreen {
     private static final float V_LEN = 1.5f;
 
     private Texture img;
-    private Vector2 touch;
     private Vector2 pos;
-    private Vector2 v;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        touch = new Vector2();
         pos = new Vector2();
-        v = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
         batch.end();
-        if(pos.dst(touch) <= v.len()){
-            pos.set(touch);
-        } else {
-            pos.add(v);
-        }
     }
 
     @Override
@@ -44,9 +35,8 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength2(V_LEN);
-        return super.touchDown(screenX, screenY, pointer, button);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        pos.set(touch);
+        return super.touchDown(touch, pointer, button);
     }
 }
