9ed733e5efa24079eda4bb6cec5eed242038d643
Andre <ncl8@mail.ru>
2021-10-22 23:22:29 +0300

    Stars

--- core/src/gb/ru/screen/MenuScreen.java
+++ core/src/gb/ru/screen/MenuScreen.java
@@ -2,57 +2,83 @@ package gb.ru.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import gb.ru.base.BaseScreen;
 import gb.ru.math.Rect;
 import gb.ru.sprite.Background;
+import gb.ru.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final int STAR_COUNT = 256;
     private static final float V_LEN = 1.5f;
 
+    private TextureAtlas atlas;
     private Texture bg;
-    private Texture img;
-    private Vector2 pos;
+    private Star stars[];
+
 
     private Background background;
 
     @Override
     public void show() {
         super.show();
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++){
+            stars[i] = new Star(atlas);
+        }
 
-        img = new Texture("badlogic.jpg");
-        pos = new Vector2();
-    }
-
-    @Override
-    public void render(float delta) {
-        super.render(delta);
-        batch.begin();
-        background.draw(batch);
-        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        super.dispose();
-        bg.dispose();
-        img.dispose();
-    }
-
-    @Override
-    public boolean touchDown(Vector2 touch, int pointer, int button) {
-        pos.set(touch);
-        return super.touchDown(touch, pointer, button);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for(Star star: stars){
+            star.resize(worldBounds);
+        }
     }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        draw();
+
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+        atlas.dispose();
+    }
+
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
+    }
+
+    private void update(float delta){
+        for(Star star: stars){
+            star.update(delta);
+        }
+    }
+
+    private void draw(){
+        batch.begin();
+        background.draw(batch);
+        for(Star star: stars){
+            star.draw(batch);
+        }
+        batch.end();
+    }
+
+
 }
